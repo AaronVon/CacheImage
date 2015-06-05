9cf716033f9a29cf228b9a128adc291ccd28f03c
Aaron <pioneervon@gmail.com>
2015-06-05 23:26:11 +0800

    my imagecache test

---
+++ app/src/main/java/com/pioneer/aaron/cacheimage/MainActivity.java
@@ -0,0 +1,57 @@
+package com.pioneer.aaron.cacheimage;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DefaultConfigurationFactory;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class MainActivity extends ActionBarActivity {
+    String imgUrl = "http://img0.bdstatic.com/img/image/4a75a05f8041bf84df4a4933667824811426747915.jpg";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*AsyncImageLoader imageLoader = new AsyncImageLoader();
+        imageLoader.loadImage(imgUrl, (ImageView) findViewById(R.id.imageView));*/
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .build();
+        ImageLoaderConfiguration imageLoaderConfiguration = new ImageLoaderConfiguration.Builder(this)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(imageLoaderConfiguration);
+        imageLoader.displayImage(imgUrl, (ImageView) findViewById(R.id.imageView));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
